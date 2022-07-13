9bbee348fb842e919ad8c21c597ec7391fe01023
楓 紅桜 <rngkq545@gmail.com>
2022-07-13 16:49:35 +0900

    gitpushエラー

--- DirectX12CG/Util.h
+++ DirectX12CG/Util.h
@@ -16,7 +16,7 @@ namespace MCB
 	int Abs(int num);
 	float Abs(float num);
 	double Abs(double num);
-	float ConvertRadian(float angle);
+	float ConvertRadius(float angle);
 
 	void InitRand();
 	int GetRand(int min, int max);
